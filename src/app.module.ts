a2fcc4bb34f67951c086db6907d3f66648fe4053
KHab1bull0 <habibullotoyrilmasov@gmail.com>
2024-07-12 09:56:24 +0500

    First commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,38 +5,33 @@ import { UserCoursesModule } from './user-courses/user-courses.module';
 import { CourseFilesModule } from './course-files/course-files.module';
 import { CourseModule } from './course/course.module';
 import { AuthModule } from './auth/auth.module';
-
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { Log } from './log/entities/log.entity';
 
 
 
 @Module({
   imports: [
-    imports: [
-      ConfigModule.forRoot({
-        envFilePath: '.env',
-        isGlobal: true,
-      }),
-      SequelizeModule.forRoot({
-        dialect: process.env.DIALECT as any,
-        host: process.env.HOST,
-        port: parseInt(process.env.PORT_DB, 10),
-        username: 'postgres',
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
-        autoLoadModels: process.env.AUTO_LOAD_MODELS === 'true',
-        synchronize: process.env.SYNCHRONIZE === 'true',
-        logging:false
-      }),
-      SequelizeModule.forFeature([Log]),
-      AuthModule,
-      ProductsModule,
-      CustomersModule,
-      OrdersModule,
-      OrderProductsModule,
-      RefreshTokenModule,
-    ],
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: process.env.DIALECT as any,
+      host: process.env.HOST,
+      port: parseInt(process.env.PORT_DB, 10),
+      username: 'postgres',
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      autoLoadModels: process.env.AUTO_LOAD_MODELS === 'true',
+      synchronize: process.env.SYNCHRONIZE === 'true',
+      logging: false
+    }),
+    SequelizeModule.forFeature([Log]),
+
     UserModule, CourseModule, FilesModule, , UserCoursesModule, CourseFilesModule, AuthModule
-    
+
   ],
   controllers: [],
   providers: [],
